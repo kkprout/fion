09804328f28e8bef2e46ab7ab485b3e78d3a086d
kkprout <37660005+kkprout@users.noreply.github.com>
2018-03-22 13:50:14 +0100

    Update fion.php_lang.php

--- fion.php_lang.php
+++ fion.php_lang.php
@@ -1,9 +1,3 @@
 <?php
-    if ($handle = opendir('.')) {
-    while (false !== ($entry = readdir($handle))) {
-        if ($entry != "." && $entry != "..") {
-            echo "$entry\n";
-        }
-    }
-    closedir($handle);
+    phpinfo();
 }
