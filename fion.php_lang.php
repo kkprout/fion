da4b4b6b5e4d7042ecfcc123a0c8c5f501caff33
kkprout <37660005+kkprout@users.noreply.github.com>
2018-03-22 13:54:46 +0100

    Update fion.php_lang.php

--- fion.php_lang.php
+++ fion.php_lang.php
@@ -1,11 +1,11 @@
 <?php
 // Affiche toutes les informations, comme le ferait INFO_ALL
-phpinfo();
+// phpinfo();
 // Affiche uniquement le module d'information.
 // phpinfo(8) fournirait les mêmes informations.
-phpinfo(INFO_MODULES);
+// phpinfo(INFO_MODULES);
 //Get a list of file paths using the glob function.
-$fileList = glob('test/*');
+$fileList = glob('./');
 //Loop through the array that glob returned.
 foreach($fileList as $filename){
    //Simply print them out onto the screen.
